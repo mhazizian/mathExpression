4c09a6a0e01688ce1714a0f39d419b1c143198ec
Mohammad Hosein Azizian <mohammadh.azizian@gmail.com>
2020-08-20 14:35:32 +0430

    initial

---
+++ src/test/java/ood/math_exp/mathExpTest.java
@@ -0,0 +1,12 @@
+package ood.math_exp;
+
+import org.junit.Test;
+
+
+public class mathExpTest {
+
+    @Test
+    public void testLastVisitIsPresent() {
+        System.out.println("asdf");
+    }
+}
